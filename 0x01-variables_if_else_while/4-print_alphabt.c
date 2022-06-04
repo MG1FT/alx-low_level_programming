6dc1506d3e8bcfa7e536f2349d82518cd79bbcba
gift <gift.matshika3@gmail.com>
2022-06-03 17:44:01 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z' ; r++)
+	{
+		putchar(r);
+	}
+
+	putchar('\n');
+	return (0);
+}
