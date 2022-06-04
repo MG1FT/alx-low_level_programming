05741cf13411781ca85142dd89077160e97cda9d
gift <gift.matshika3@gmail.com>
2022-06-03 18:01:51 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b < '9' ; b++)
+	{
+		printf("%d\n", b);
+	}
+	return (0);
+}
