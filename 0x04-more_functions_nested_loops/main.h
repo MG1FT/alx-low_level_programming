b7273160d5f53f72be14df293f9adca74bdf4519
gift <gift.matshika3@gmail.com>
2022-06-09 14:50:21 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+/**
+ * Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
+
+Each number or word should be separated by a space
+You are allowed to use the standard library
+*/
+void print_triangle(int size);
+#endif /* MAIN_H */
