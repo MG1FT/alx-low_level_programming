4f799d4d779e69be41ebc00e303cd15b1e335efa
gift <gift.matshika3@gmail.com>
2022-06-14 07:26:23 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ *
+ * Description: strlen similiar function to return length
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+	}
+	return (x);
+}
