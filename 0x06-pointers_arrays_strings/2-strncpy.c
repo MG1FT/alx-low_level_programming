1e64e355b8e624974f4b1abe0b7d19a493bbb52f
gift <gift.matshika3@gmail.com>
2022-06-16 12:54:20 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - two words
+ * @dest : pointer to char params
+ * @src : pointer to char params
+ * @n : int params
+ * 
+ * Return: *dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0' && x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	for ( ; x < n; x++)
+	{
+		dest[x] = '\0';
+	}
+
+	return (dest);
+}
